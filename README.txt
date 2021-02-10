8a7325026f4488eb019b19c743be2cdc3bff9d4d
Enes Kaya <enes.kaya95@hotmail.com>
2021-02-10 16:11:32 +0300

    moveTask, createTask, get specificTable tasks and get all tasks are implemented

--- README.txt
+++ README.txt
@@ -15,4 +15,7 @@ Database
 Curls
     -> curl -X POST http://127.0.0.1:5000/get/1
     -> curl -X GET http://127.0.0.1:5000/get/4
-    -> curl -d '{"content" : "nes"}'  -H 'Content-Type: application/json'  http://127.0.0.1:5000/create
+    -> curl -d '{"content" : "nes"}' -H 'Content-Type: application/json' -X POST  http://127.0.0.1:5000/createTask
+    -> curl -X GET http://127.0.0.1:5000
+    -> curl -d '{"tableName" : "DONE"}'  -H 'Content-Type: application/json'  -X GET http://127.0.0.1:5000/getTasks
+    -> 
--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect ,jsonify
 from flask_sqlalchemy import SQLAlchemy
-from enum import Enum
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
@@ -10,7 +9,7 @@ db=SQLAlchemy(app)
 
 class Task(db.Model):
     id = db.Column(db.Integer , primary_key=True)
-    tableId = db.Column(db.Integer , default=0)
+    tableName = db.Column(db.String(200) , default="TODO")
     content = db.Column(db.String(200) , nullable=False)
     completed = db.Column(db.Integer , default=0)
 
@@ -18,8 +17,8 @@ class Task(db.Model):
         return '<Task %r>' %self.id
 
 class TodoTask(db.Model):
-    __tablename__ = 'TODO Tasks'
-    taskId = db.Column(db.Integer , primary_key=True)
+    id = db.Column(db.Integer , primary_key=True)
+    taskId = db.Column(db.Integer )
     content = db.Column(db.String(200) , nullable=False)
     completed = db.Column(db.Integer , default=0)
 
@@ -28,8 +27,8 @@ class TodoTask(db.Model):
 
 
 class InProgressTask(db.Model):
-    __tablename__ = 'In Progress Tasks'
-    taskId = db.Column(db.Integer , primary_key=True)
+    id = db.Column(db.Integer , primary_key=True)
+    taskId = db.Column(db.Integer )
     content = db.Column(db.String(200) , nullable=False)
     completed = db.Column(db.Integer , default=0)
 
@@ -37,8 +36,8 @@ class InProgressTask(db.Model):
         return '<In Progress Task %r>' %self.taskId
 
 class DoneTask(db.Model):
-    __tablename__ = 'DONE Tasks'
-    taskId = db.Column(db.Integer , primary_key=True)
+    id = db.Column(db.Integer , primary_key=True)
+    taskId = db.Column(db.Integer )
     content = db.Column(db.String(200) , nullable=False)
     completed = db.Column(db.Integer , default=0)
 
@@ -50,33 +49,32 @@ class DoneTask(db.Model):
 @app.route('/', methods=['POST' ,'GET'])
 def index():
 
-    if request.method == 'POST':
-        task_content = request.form['content']
-        new_task =Task(content="he")
-
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-
-        except:
-            return "error"
+    if request.method == 'GET':
+        tasks = Task.query.order_by(Task.id).all()
+        all_tasks = [{'id':task.id,'content':task.content, 'tableName':task.tableName } for task in tasks]
+        return jsonify(all_tasks)
+ 
     else:
-        tasks = Todo.query.order_by(Todo.id).all()
+        pass
+        
 
 
-    return render_template('index.html')
 
 
-@app.route('/create', methods=['POST' ,'GET'])
-def create():
+@app.route('/createTask', methods=['POST' ,'GET'])
+def createTask():
 
     if request.method == 'POST':
 
         data = request.get_json()
-        new_task=Task(content=data["content"]) #
-
+        new_task=Task(content=data["content"], tableName="TODO") 
+        
         try:
             db.session.add(new_task)
+            db.session.commit()
+
+            todo_task=TodoTask(taskId=new_task.id,content=new_task.content)
+            db.session.add(todo_task)
             db.session.commit()
             return "The task with id = %d is added" %new_task.id
 
@@ -86,13 +84,76 @@ def create():
         pass
 
 
-@app.route('/get/<int:id>', methods=['POST' ,'GET'])
-def get(id):
+@app.route('/getTasks/<string:table>', methods=['POST' ,'GET'])
+def getTasks(table):
+
+    if request.method == 'GET':
+        if table == "TODO":
+            tasks = TodoTask.query.order_by(TodoTask.id).all()
+        elif table == "INPROGRESS":
+            tasks = InProgressTask.query.order_by(InProgressTask.id).all()
+        elif table == "DONE":
+            tasks = DoneTask.query.order_by(DoneTask.id).all()
+        
+        
+        all_tasks = [{'content':task.content,'id':task.taskId} for task in tasks]
+        return jsonify(all_tasks)
+    else:
+        pass
+
+
+
+
+@app.route('/moveTask/<int:id>/<string:toTable>', methods=['POST' ,'GET'])
+def moveTask(id,toTable):
 
     if request.method == 'GET':
         task_get =Task.query.get_or_404(id)
+        
+        
+        try:
             
-        return  jsonify(content=task_get.content) 
+            if task_get.tableName == "TODO":
+                task_remove = TodoTask.query.filter_by(taskId=task_get.id).first()
+            elif task_get.tableName == "INPROGRESS":
+                task_remove =InProgressTask.query.filter_by(taskId=task_get.id).first()
+            elif task_get.tableName == "DONE":
+                task_remove =DoneTask.query.filter_by(taskId=task_get.id).first()
+
+            
+            
+            #delete old table
+            db.session.delete(task_remove)
+            db.session.commit()
+            
+            
+            #add to new table
+            try:
+                if toTable == "TODO":
+                    task_add =TodoTask(taskId=task_get.id,content=task_get.content)
+                elif toTable == "INPROGRESS":
+                    task_add =InProgressTask(taskId=task_get.id,content=task_get.content)
+                elif toTable == "DONE":
+                    task_add =DoneTask(taskId=task_get.id,content=task_get.content)
+
+                db.session.add(task_add)
+                db.session.commit()
+
+
+                # change Task Table
+                try:
+                    task_get.tableName = toTable
+                    db.session.commit()
+                    return jsonify(result = "SUCCES")
+                except: 
+                    return "ERROR 1"
+
+            except: 
+                return "ERROR 2"
+
+            
+        except:
+            return "ERROR 3"
     else:
         pass
 
