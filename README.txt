f8322049f5b46017734c9da542b3fa84e32025a0
Enes Kaya <enes.kaya95@hotmail.com>
2021-02-10 13:15:36 +0300

    database arranged

--- README.txt
+++ README.txt
@@ -10,3 +10,9 @@ Virtual enviroment
 Database
     -> from app import db
     -> db.create_all()
+    
+
+Curls
+    -> curl -X POST http://127.0.0.1:5000/get/1
+    -> curl -X GET http://127.0.0.1:5000/get/4
+    -> curl -d '{"content" : "nes"}'  -H 'Content-Type: application/json'  http://127.0.0.1:5000/create
--- app.py
+++ app.py
@@ -1,26 +1,58 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect ,jsonify
 from flask_sqlalchemy import SQLAlchemy
+from enum import Enum
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 
 db=SQLAlchemy(app)
 
-class Todo(db.Model):
+
+class Task(db.Model):
     id = db.Column(db.Integer , primary_key=True)
+    tableId = db.Column(db.Integer , default=0)
     content = db.Column(db.String(200) , nullable=False)
     completed = db.Column(db.Integer , default=0)
 
     def __repr__(self):
         return '<Task %r>' %self.id
 
-@app.route('/', methods=['POST' ,'GET'])
+class TodoTask(db.Model):
+    __tablename__ = 'TODO Tasks'
+    taskId = db.Column(db.Integer , primary_key=True)
+    content = db.Column(db.String(200) , nullable=False)
+    completed = db.Column(db.Integer , default=0)
 
+    def __repr__(self):
+        return '<TODO Task %r>' %self.taskId
+
+
+class InProgressTask(db.Model):
+    __tablename__ = 'In Progress Tasks'
+    taskId = db.Column(db.Integer , primary_key=True)
+    content = db.Column(db.String(200) , nullable=False)
+    completed = db.Column(db.Integer , default=0)
+
+    def __repr__(self):
+        return '<In Progress Task %r>' %self.taskId
+
+class DoneTask(db.Model):
+    __tablename__ = 'DONE Tasks'
+    taskId = db.Column(db.Integer , primary_key=True)
+    content = db.Column(db.String(200) , nullable=False)
+    completed = db.Column(db.Integer , default=0)
+
+    def __repr__(self):
+        return '<Done Task %r>' %self.taskId
+
+
+
+@app.route('/', methods=['POST' ,'GET'])
 def index():
 
     if request.method == 'POST':
         task_content = request.form['content']
-        new_task =Todo(content=task_content)
+        new_task =Task(content="he")
 
         try:
             db.session.add(new_task)
@@ -28,13 +60,42 @@ def index():
 
         except:
             return "error"
-
-
     else:
         tasks = Todo.query.order_by(Todo.id).all()
-        
+
 
     return render_template('index.html')
+
+
+@app.route('/create', methods=['POST' ,'GET'])
+def create():
+
+    if request.method == 'POST':
+
+        data = request.get_json()
+        new_task=Task(content=data["content"]) #
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return "The task with id = %d is added" %new_task.id
+
+        except:
+            return "The task can not be added %s" %data["content"]
+    else:
+        pass
+
+
+@app.route('/get/<int:id>', methods=['POST' ,'GET'])
+def get(id):
+
+    if request.method == 'GET':
+        task_get =Task.query.get_or_404(id)
+            
+        return  jsonify(content=task_get.content) 
+    else:
+        pass
+
 
 if __name__ == "__main__":
     app.run(debug=True)
